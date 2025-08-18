a3ee79b286a7f4cece98b2e88a71c3278b66967a
Jure Rebernik <jure.rebernik@luxonis.com>
2025-08-18 14:04:58 +0200

    uvc: fix shellcheck warnings
    Signed-off-by: Jure Rebernik <jure.rebernik@luxonis.com>

--- streaming/uvc/uvc-start.sh
+++ streaming/uvc/uvc-start.sh
@@ -6,8 +6,8 @@ VID="0x05c6"
 PID="0x90cb" 
 
 # find serialno from cmdline
-SERIAL=`cat /proc/cmdline | grep -o "androidboot.serialno=[A-Za-z0-9]*" | cut -d "=" -f2`
-if [ "$SERIAL" == "" ]; then
+SERIAL=$(grep -o "androidboot.serialno=[A-Za-z0-9]*" /proc/cmdline | cut -d "=" -f2)
+if [ -z "$SERIAL" ]; then
     echo "SERIAL not found with cmdline, use default"
     SERIAL="12345678"
 fi
@@ -29,7 +29,7 @@ remove_all_gadgets() {
         return 0
     fi
 
-    pushd /sys/kernel/config/usb_gadget/g1
+    pushd /sys/kernel/config/usb_gadget/g1 >/dev/null
 
     echo "Unbinding USB Device Controller..."
     retries=0
@@ -81,7 +81,7 @@ remove_all_gadgets() {
 
     rmdir strings/0x409
 
-    popd
+    popd >/dev/null
 
     rmdir /sys/kernel/config/usb_gadget/g1
 
@@ -105,34 +105,34 @@ create_frame() {
 
     wdir=functions/$FUNCTION/streaming/$FORMAT/$NAME/${HEIGHT}p
 
-    mkdir -p $wdir
-    echo $WIDTH > $wdir/wWidth
-    echo $HEIGHT > $wdir/wHeight
-    echo $(( $WIDTH * $HEIGHT * 2 )) > $wdir/dwMaxVideoFrameBufferSize
-    cat <<EOF > $wdir/dwFrameInterval
+    mkdir -p "$wdir"
+    echo "$WIDTH" > "$wdir/wWidth"
+    echo "$HEIGHT" > "$wdir/wHeight"
+    echo $(( WIDTH * HEIGHT * 2 )) > "$wdir/dwMaxVideoFrameBufferSize"
+    cat <<EOF > "$wdir/dwFrameInterval"
 333333
 EOF
 }
 
 create_uvc() {
     # Example usage:
-    #	create_uvc <target config> <function name>
-    #	create_uvc config/c.1 uvc.0
+    #   create_uvc <target config> <function name>
+    #   create_uvc config/c.1 uvc.0
     CONFIG=$1
     FUNCTION=$2
 
-    echo "	Creating UVC gadget functionality : $FUNCTION"
-    mkdir functions/$FUNCTION
+    echo "    Creating UVC gadget functionality : $FUNCTION"
+    mkdir "functions/$FUNCTION"
 
-    # create_frame $FUNCTION 640 360 uncompressed u
-    # create_frame $FUNCTION 1280 720 uncompressed u
-    # create_frame $FUNCTION 320 180 uncompressed u
-    create_frame $FUNCTION 1920 1080 mjpeg m
-    # create_frame $FUNCTION 640 480 mjpeg m
-    # create_frame $FUNCTION 640 360 mjpeg m
+    # create_frame "$FUNCTION" 640 360 uncompressed u
+    # create_frame "$FUNCTION" 1280 720 uncompressed u
+    # create_frame "$FUNCTION" 320 180 uncompressed u
+    create_frame "$FUNCTION" 1920 1080 mjpeg m
+    # create_frame "$FUNCTION" 640 480 mjpeg m
+    # create_frame "$FUNCTION" 640 360 mjpeg m
 
-    mkdir functions/$FUNCTION/streaming/header/h
-    cd functions/$FUNCTION/streaming/header/h
+    mkdir "functions/$FUNCTION/streaming/header/h"
+    cd "functions/$FUNCTION/streaming/header/h"
     # ln -s ../../uncompressed/u
     ln -s ../../mjpeg/m
     cd ../../class/fs
@@ -147,19 +147,28 @@ create_uvc() {
     ln -s header/h class/ss
     cd ../../../
 
-    echo 3072 > functions/$FUNCTION/streaming_maxpacket
+    echo 3072 > "functions/$FUNCTION/streaming_maxpacket"
 
-    ln -s functions/$FUNCTION configs/c.1
+    ln -s "functions/$FUNCTION" configs/c.1
 }
+
+terminate() {
+    if [ "$child_pid" -gt 0 ] 2>/dev/null && kill -0 "$child_pid" 2>/dev/null; then
+    kill -TERM "$child_pid"
+    wait "$child_pid" || true
+    fi
+    exit 143
+}
+trap terminate INT TERM
 
 case "$1" in
     start)
     echo "    ==== Configuring USB gadget ===="
     sleep 5
 
-    if [ ! -d $CONFIGFS ]; then
+    if [ ! -d "$CONFIGFS" ]; then
         echo "Configfs not mounted, please mount it first"
-        exit 1;
+        exit 1
     fi
 
     remove_all_gadgets
@@ -167,26 +176,26 @@ case "$1" in
     echo "Creating the USB gadget"
 
     echo "Creating gadget directory g1"
-    mkdir -p $GADGET/g1
+    mkdir -p "$GADGET/g1"
 
-    pushd $GADGET/g1
+    pushd "$GADGET/g1" >/dev/null
     if [ $? -ne 0 ]; then
         echo "Error creating usb gadget in configfs"
-        exit 1;
+        exit 1
     else
         echo "OK"
     fi
 
     echo "Setting Vendor and Product ID's"
-    echo $VID > idVendor
-    echo $PID > idProduct
+    echo "$VID" > idVendor
+    echo "$PID" > idProduct
     echo "OK"
 
     echo "Setting English strings"
     mkdir -p strings/0x409
-    echo $SERIAL > strings/0x409/serialnumber
-    echo $MANUF > strings/0x409/manufacturer
-    echo $PRODUCT > strings/0x409/product
+    echo "$SERIAL" > strings/0x409/serialnumber
+    echo "$MANUF"  > strings/0x409/manufacturer
+    echo "$PRODUCT" > strings/0x409/product
     echo "OK"
 
     echo "Creating Config"
@@ -199,7 +208,7 @@ case "$1" in
 
     echo "Binding USB Device Controller..."
     while true; do
-        echo $UDC > UDC 2>/dev/null
+        echo "$UDC" > UDC 2>/dev/null
         if [ $? -eq 0 ]; then
             echo "Successfully bound UDC controller $UDC"
             break
@@ -207,13 +216,13 @@ case "$1" in
         sleep 1
     done
 
-    popd
+    popd >/dev/null
 
     echo "    ==== Configuration done ===="
     
     echo "    ==== Starting UVC APP ===="
     /app/uvc_example
-
+    
     ;;
 
     stop)
@@ -222,4 +231,5 @@ case "$1" in
     ;;
     *)
     echo "Usage : $0 {start|stop}"
+    ;;
 esac
